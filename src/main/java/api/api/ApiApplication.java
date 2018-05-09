468f7435cf9dc1d2663862585dbdea370d2bca5f
tokorozawa-ono <onot@tokorozawa.work>
2018-05-09 18:19:03 +0900

    テンプレート使用、不用記載削除

--- src/main/java/api/api/ApiApplication.java
+++ src/main/java/api/api/ApiApplication.java
@@ -2,21 +2,31 @@ package api.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 @SpringBootApplication
-@RestController
+@Controller
 public class ApiApplication {
 
     @Autowired
 	private CityMapper cityMapper;
 
-	@RequestMapping("/")
-	String index() {
-		City ub = this.cityMapper.findByState("JP");
-        return "はじめまして！" + ub.toString() + "です";
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	String index(Model model) {
+		City city = this.cityMapper.findByState("JP");
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy'年'MM'月'dd'日'k'時'mm'分'ss'秒'");
+        model.addAttribute("now", sdf.format(new Date()));
+        model.addAttribute("city", city.toString());
+        return "index";
 	}
 
 	public static void main(String[] args) {
